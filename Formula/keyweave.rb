c91781ba92bdc6fe61dbd9f0607417a666e489e0
Bart van der Braak <bartvdbraak@gmail.com>
2023-11-08 22:49:44 +0100

    feat: add musl option and template release

--- Formula/keyweave.rb
+++ Formula/keyweave.rb
@@ -1,7 +1,11 @@
 class Keyweave < Formula
   desc "Fetches secrets from Azure Key Vault and weaves them into a convenient .env file"
   homepage "https://github.com/bartvdbraak/keyweave"
+  license "GPLv3"
   version "0.2.0"
+
+  option "with-gnu-libc", "Install glibc binary"
+  option "with-musl-libc", "Install musl binary (default)"
 
   if OS.mac?
     if Hardware::CPU.arm?
@@ -21,8 +25,16 @@ class Keyweave < Formula
         sha256 "da415340276b6bc8f700e54cafdef23445973dba0d126065cf73135dff2c922d"
       end
     else      
-      url "https://github.com/bartvdbraak/keyweave/releases/download/v0.2.0/keyweave-x86_64-unknown-linux-musl.tar.xz"
-      sha256 "6f6ad0810a4bf75d914dd09c4c55efadc8b038d10811c4223b787ac5d1cf4186"
+      if build.with?("with-gnu-libc")
+        url "https://github.com/bartvdbraak/keyweave/releases/download/v0.2.0/keyweave-x86_64-unknown-linux-gnu.tar.xz"
+        sha256 "79317c194bd41a2d9ec55e80353e3eb510a11e3c57708e65cc0a34966a86ae14"
+      elsif build.with?("with-musl-libc")
+        url "https://github.com/bartvdbraak/keyweave/releases/download/v0.2.0/keyweave-x86_64-unknown-linux-musl.tar.xz"
+        sha256 "6f6ad0810a4bf75d914dd09c4c55efadc8b038d10811c4223b787ac5d1cf4186"
+      else
+        url "https://github.com/bartvdbraak/keyweave/releases/download/v0.2.0/keyweave-x86_64-unknown-linux-musl.tar.xz"
+        sha256 "6f6ad0810a4bf75d914dd09c4c55efadc8b038d10811c4223b787ac5d1cf4186"
+      end
     end
   end
 
